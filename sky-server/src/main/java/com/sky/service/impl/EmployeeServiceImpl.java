38ba51f5a2ea0b853619b87bd0490be104e1c5ea
rich <rich@itcast.cn>
2024-11-03 12:24:42 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -39,6 +45,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
+        //对前端传过来的密码进行md5加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         // TODO 后期需要进行md5加密，然后再进行比对
         if (!password.equals(employee.getPassword())) {
             //密码错误
@@ -52,6 +60,41 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     */
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        System.out.println("当前线程id:" +
+                Thread.currentThread().getId());
+   Employee employee = new Employee();
+//对象属性拷贝
+        BeanUtils.copyProperties(employeeDTO,employee);
+//设置默认密码123456，需要进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        //设置当前记录时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //设置创建人id
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        //设置账号的状态，默认正常状态 1表示正常 0表示锁定
+        //ENABLE:可用的
+        employee.setStatus(StatusConstant.ENABLE);
+
+
+//        //设置当前记录创建人id和修改人id
+//        employee.setCreateUser(10L);//目前写个假数据，后期修改
+//        employee.setUpdateUser(10L);
+        //执行新增操作
+        employeeMapper.insert(employee);
+
+
+
     }
 
 }
