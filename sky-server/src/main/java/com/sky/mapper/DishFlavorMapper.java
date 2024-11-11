998f3c496cd3f424a41b3a4743d062eb8d1f28aa
rich <rich@itcast.cn>
2024-11-11 20:09:11 +0800

    新增彩票代码功能代码

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,14 +1,16 @@
 package com.sky.mapper;
 
 import com.sky.annotation.AutoFill;
-import com.sky.entity.Category;
 import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
 import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
-public interface DishMapper {
+public interface DishFlavorMapper {
 
     /**
      * 根据分类id查询菜品数量
@@ -18,10 +20,11 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+
     /**
-     *  新增菜品
-     * @param dish
+     * 新增菜品，同时插入菜品对应的口味数据
+     * @param flavors
      */
-    @AutoFill(OperationType.INSERT)
-    void insert(Dish dish);
+
+    void insertBatch(List<DishFlavor> flavors);
 }
