689aa4bac9021cf33c9bfb3607277f660b62be3d
rich0807 <3428773947@qq.com>
2024-12-09 21:22:10 +0800

    查看历史订单及订单详情业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -30,4 +32,15 @@ public interface OrderMapper {
      */
     @Select("select * from orders where status = #{param1} and order_time < #{param2}")
     List<Orders> getByStatusAndOrdertimeLT(Integer pendingPayment, LocalDateTime time);
+    /**
+     * 分页条件查询并按下单时间排序
+     * @param ordersPageQueryDTO
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+    /**
+     * 根据id查询订单
+     * @param id
+     */
+    @Select("select * from orders where id=#{id}")
+    Orders getById(Long id);
 }
